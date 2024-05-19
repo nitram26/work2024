00b6f6fc79be28275e1dc98b66347caea54f795a
cmncp <carlos.cervera@istpmala.edu.pe>
2024-05-18 20:33:09 -0500

    optimizado html

--- src/views/FrontView.vue
+++ src/views/FrontView.vue
@@ -1,8 +1,14 @@
 <template>
   <v-container :fluid="true">
     <v-row>
+      <v-col cols="12" >
+        <h2 class="bg-blue-darken-4 pa-4">Vue2 MODELS -TEXT HTML</h2>
+      <p class="bg-orange-lighten-4 pa-4 mb-4">Modelos en proceso de optimización, solo para pruebas. Buscar un modelo que sirva para todos lo casos </p>
+      </v-col>
+    </v-row>
+      <v-row>
       <v-col cols="3" class="bg-teal-lighten-5">
-        <h2>Vue2</h2>
+<!-- ****************************************************************************** -->
         <v-card
     class="mx-auto my-8"
     elevation="16"
@@ -10,17 +16,16 @@
     href="http://127.0.0.1:5500/src/assets/ModelVue2.html"
     max-width="344" >
     <v-card-item>
-      <v-card-title> Model Vue2</v-card-title>
+      <v-card-title> Model </v-card-title>
       <v-card-subtitle>Proyectos Sursa</v-card-subtitle>
     </v-card-item>
-
     <v-card-text>
     Modelo básico para los model de Vue2
     </v-card-text>
   </v-card> 
       </v-col>
+<!-- ****************************************************************************** -->
       <v-col cols="3" class="bg-teal-lighten-5">
-        <h2>Vue2</h2>
         <v-card
     class="mx-auto my-8"
     elevation="16"
@@ -28,15 +33,70 @@
     href="http://127.0.0.1:5500/src/assets/ValidacionesVue2.html"
     max-width="344" >
     <v-card-item>
-      <v-card-title> Model Vue2</v-card-title>
+      <v-card-title> Validaciones </v-card-title>
       <v-card-subtitle>Proyectos Sursa</v-card-subtitle>
     </v-card-item>
-
     <v-card-text>
-    Modelo básico para los model de Vue2
+      Validaciones para el Front y Back
     </v-card-text>
   </v-card> 
-      </v-col>
+</v-col>
+<!-- ****************************************************************************** -->
+      <v-col cols="3" class="bg-teal-lighten-5">
+        <v-card
+    class="mx-auto my-8"
+    elevation="16"
+    target="_blank"
+    href="http://127.0.0.1:5500/src/assets/ControladorVue2.html"
+    max-width="344" >
+    <v-card-item>
+      <v-card-title> Controller </v-card-title>
+      <v-card-subtitle>Página completa</v-card-subtitle>
+    </v-card-item>
+    <v-card-text>
+      Código total de una página 
+    </v-card-text>
+  </v-card> 
+</v-col>
+<!-- ********************************************************************** -->
+      <v-col cols="3" class="bg-teal-lighten-5">
+        <v-card
+    class="mx-auto my-8"
+    elevation="16"
+    target="_blank"
+    href="http://127.0.0.1:5500/src/assets/PagesVue2.html"
+    max-width="344" >
+    <v-card-item>
+      <v-card-title> Pages </v-card-title>
+      <v-card-subtitle>Página completa</v-card-subtitle>
+    </v-card-item>
+    <v-card-text>
+1.	Creada la pagina la podemos llamar en la PC <br>
+2.	La ruta de llamada debe coincidir con la ruta<br>
+3.	En el headers adecuar los nombres de acuerdo al modelo y la bd<br>
+    </v-card-text>
+  </v-card> 
+</v-col>
+<!-- ********************************************************************** -->
+      <v-col cols="3" class="bg-teal-lighten-5">
+        <v-card
+    class="mx-auto my-8"
+    elevation="16"
+    target="_blank"
+    href="http://127.0.0.1:5500/src/assets/ComponentVue2.html"
+    max-width="344" >
+    <v-card-item>
+      <v-card-title> Component </v-card-title>
+      <v-card-subtitle>Página completa</v-card-subtitle>
+    </v-card-item>
+    <v-card-text>
+      1.	Cambiar mayúsculas y minúsculas <br>
+      2.	En los model las rutas y los nombres, línea 20 <br>
+      3.	Cambiar la ruta en controlador líneas 45 y 47<br>
+    </v-card-text>
+  </v-card> 
+</v-col>
+<!-- ********************************************************************** -->
        </v-row>
   </v-container>
 
