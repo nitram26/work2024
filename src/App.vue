36f4f3e037b1e0871b13826c2b166c69676c8c2c
cmncp <carlos.cervera@istpmala.edu.pe>
2024-05-20 20:48:16 -0500

    casiBien

--- src/App.vue
+++ src/App.vue
@@ -12,27 +12,23 @@
     <v-img src="@/assets/image/CERVERA.png" alt="My Image2" style="width: 39px; border-radius: 1em;"/></v-btn>
   </v-app-bar-nav-icon>
     <!-- homeFront ***********************************************************-->
-  <v-app-bar-nav-icon class="ma-12"><v-btn icon="mdi-arrange-bring-to-front" to="/homeFront"></v-btn>Front</v-app-bar-nav-icon>
+  <v-app-bar-nav-icon class="ma-14"><v-btn icon="mdi-arrange-bring-to-front" to="/homeFront"></v-btn>Traductor</v-app-bar-nav-icon>
   <!-- Back ***********************************************************-->
-  <v-app-bar-nav-icon class="ma-12"><v-btn icon="mdi-arrange-send-to-back" to="/backend"></v-btn>
-    Back</v-app-bar-nav-icon>
+  <v-app-bar-nav-icon class="ma-14"><v-btn icon="mdi-arrange-send-to-back" to="/backend"></v-btn>
+    Chat GPT</v-app-bar-nav-icon>
     <!-- Data *********************************************************-->
-  <v-app-bar-nav-icon class="ma-12"><v-btn icon="mdi-database-cog-outline" to="/data"></v-btn>Data</v-app-bar-nav-icon>
+  <v-app-bar-nav-icon class="ma-14"><v-btn icon="mdi-database-cog-outline" to="/data"></v-btn>Vuetify</v-app-bar-nav-icon>
     <!-- Data *********************************************************-->
-  <v-app-bar-nav-icon class="ma-12"><v-btn icon="mdi-store" to="/store"></v-btn>Store</v-app-bar-nav-icon>
+  <v-app-bar-nav-icon class="ma-14"><v-btn icon="mdi-store" to="/store"></v-btn>M Design</v-app-bar-nav-icon>
 </v-app-bar>
     <!-- Navigation Drawer ********************************************************* -->
  <v-navigation-drawer
-      v-model="drawer"
-      class="br-teal-darken-4"
-      theme="dark"
-      app
-    >
+      v-model="drawer" class="br-teal-darken-4" theme="dark" >
       <v-list-item link to="/" title="Home" prepend-icon="mdi-grid"></v-list-item>
-      <v-list-item link :to="ruta1" :title="nombre1" prepend-icon="mdi-arrange-bring-to-front"></v-list-item>
-      <v-list-item link :to="ruta2" :title="nombre2" prepend-icon="mdi-arrange-send-to-back"></v-list-item>
-      <v-list-item link :to="ruta3" :title="nombre3" prepend-icon="mdi-database-cog-outline"></v-list-item>
-      <v-list-item link :to="ruta4" :title="nombre4" prepend-icon="mdi-store"></v-list-item>
+      <v-list-item link to="/front" title="VUE2" prepend-icon="mdi-arrange-bring-to-front"></v-list-item>
+      <v-list-item link to="/vue2" title="VUE3" prepend-icon="mdi-arrange-send-to-back"></v-list-item>
+      <v-list-item link to="/homeFront" title="Home Front" prepend-icon="mdi-database-cog-outline"></v-list-item>
+      <v-list-item link to="/backend" title="BACK" prepend-icon="mdi-store"></v-list-item>
     </v-navigation-drawer>
     <!-- Main  y router*********************************************************-->
    <v-main>
--- src/views/Vue3View.vue
+++ src/views/Vue3View.vue
@@ -1,13 +1,42 @@
+
 <template>
-    <div>
-        <h1>VUE3</h1>
-    </div>
+  <v-app>
+   
+<span class="text-right">
+
+  <!-- Botón para abrir/cerrar el navigation drawer -->
+  <v-btn icon @click="toggleDrawer" >
+    <v-icon>{{ drawer ? 'mdi-close' : 'mdi-menu' }}</v-icon>
+  </v-btn>
+</span >
+   
+
+    <!-- Navigation Drawer -->
+    <v-navigation-drawer v-model="drawer" class="br-teal-darken-4" theme="dark">
+      <v-list>
+        <v-list-item link to="/" title="Home" prepend-icon="mdi-grid"></v-list-item>
+        <v-list-item link to="/front" title="uno" prepend-icon="mdi-arrange-bring-to-front"></v-list-item>
+        <v-list-item link to="/vue2" title="dos" prepend-icon="mdi-arrange-send-to-back"></v-list-item>
+        <v-list-item link to="/store" title="tres" prepend-icon="mdi-database-cog-outline"></v-list-item>
+        <v-list-item link to="/backend" title="cuatro" prepend-icon="mdi-store"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- Contenido de la aplicación -->
+    <v-main>
+      <!-- Contenido principal de la aplicación -->
+    </v-main>
+  </v-app>
 </template>
-<!-- ************************************* -->
+
 <script setup>
+import { ref } from 'vue';
 
+// Variable para controlar la apertura y cierre del navigation drawer
+const drawer = ref(false);
+
+// Función para alternar entre abrir y cerrar el navigation drawer
+const toggleDrawer = () => {
+  drawer.value = !drawer.value;
+};
 </script>
-<!-- ************************************* -->
-<style scoped>
-
-</style>
