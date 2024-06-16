2fc98f765284dac9ba25fba0417dd05c6a5ecb6a
cmncp <carlos.cervera@istpmala.edu.pe>
2024-06-16 15:27:19 -0500

    navigations

--- src/views/WorkView.vue
+++ src/views/WorkView.vue
@@ -1,33 +1,147 @@
 <template>
+  
+  <h2 class="bg-yellow-lighten-4 pa-4">NAVIGATION DRAWER <br>SCRIPT</h2>
+    <p class="bg-orange-lighten-4 pa-4 mb-4">Desaparece el listado de enlaces</p>
+  <v-card>
+    <v-layout>
+      <v-app-bar
+        color="primary"
+        prominent
+      >
+        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
 
-    <h1>VALORES COMPUTADOS</h1>
-<v-row>
-  <v-col cols="2">
+        <v-toolbar-title>ARCHIVOS</v-toolbar-title>
 
-<v-text-field
-  v-model="input"
-  placeholder="Escriba aquí"
-  outlined
-  dense
+        <v-spacer></v-spacer>
 
-/>
-<h2>{{ input }}</h2>
-</v-col>
-</v-row>
+        <template v-if="$vuetify.display.mdAndUp">
+          <v-btn icon="mdi-magnify" variant="text"></v-btn>
 
+          <v-btn icon="mdi-filter" variant="text"></v-btn>
+        </template>
+
+        <v-btn icon="mdi-dots-vertical" variant="text"></v-btn>
+      </v-app-bar>
+
+      <v-navigation-drawer
+        v-model="drawer"
+        :location="$vuetify.display.mobile ? 'bottom' : undefined"
+        temporary
+      >
+        <v-list
+          :items="items"
+        ></v-list>
+      </v-navigation-drawer>
+
+      <v-main style="height: 500px;">
+        <v-card-text>
+          El cajón de navegación aparecerá desde la parte inferior en pantallas de menor tamaño. 
+        </v-card-text>
+      </v-main>
+    </v-layout>
+  </v-card>
+  <v-row>
+    <v-col cols="12" class="mt-8">
+      <h2 class="bg-yellow-lighten-4 pa-4">EXPANDED ON HOVER <br>NO SCRIPT</h2>
+      <p class="bg-orange-lighten-4 pa-4 mb-4">Se reduce el listado de enlaces</p>
+    </v-col>
+  </v-row>
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer
+        expand-on-hover
+        rail
+      >
+        <v-list>
+          <v-list-item
+            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+            subtitle="sandra_a88@gmailcom"
+            title="Sandra Adams"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
+          <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main style="height: 250px"></v-main>
+    </v-layout>
+  </v-card>
+  <!-- FOOTER /////////////////////////////////////////-->
+  <v-row >
+    <v-col cols="12" class="mt-8">
+      <h2 class="bg-yellow-lighten-4 pa-4">FOOTER <br> SCRIPT</h2>
+      <p class="bg-orange-lighten-4 pa-4 mb-4">Se reduce el listado de enlaces</p>
+    </v-col>
+  </v-row>
+  <v-row>
+     <v-footer class="d-flex flex-column">
+    <div class="bg-teal d-flex w-100 align-center px-4">
+      <strong>CONEXIÓN A REDES SOCIALES </strong>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        v-for="icon in icons"
+        :key="icon"
+        :icon="icon"
+        class="mx-4"
+        size="small"
+        variant="plain"
+      ></v-btn>
+    </div>
+
+    <div class="px-4 py-2 bg-black text-center w-100">
+      {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+    </div>
+  </v-footer>
+  </v-row>
 </template>
-<!-- ******************************* -->
+<!-- ******************************* --> 
 <script setup>
-import { ref } from 'vue';
+// BOTTOM DRAWER
+import { ref, watch } from 'vue';
 
+const drawer = ref(false);
+const group = ref(null);
+const items = [
+  {
+    title: 'Foo',
+    value: 'foo',
+  },
+  {
+    title: 'Bar',
+    value: 'bar',
+  },
+  {
+    title: 'Fizz',
+    value: 'fizz',
+  },
+  {
+    title: 'Buzz',
+    value: 'buzz',
+  },
+];
 
-let input = ref('')
+watch(group, () => {
+  drawer.value = false;
+});
 
+//FOOTER
+const icons = ref([
+  'mdi-facebook',
+  'mdi-twitter',
+  'mdi-linkedin',
+  'mdi-instagram',
+])
 </script>
+
 <!-- ******************************* -->
 <style scoped>
-div{
-  background-color: antiquewhite;
 
-}
 </style>
--- src/views/vuetify/navigation/NavigationView.vue
+++ src/views/vuetify/navigation/NavigationView.vue
@@ -13,62 +13,128 @@
   <v-navigation-drawer v-model="drawer" class="br-teal-darken-4" theme="dark">
       <v-list>
         <v-list-item link to="/" title="HOME" prepend-icon="mdi-grid"></v-list-item>
-        <v-list-item ><v-btn @click="scrollTo('section1')" prepend-icon="mdi-arrange-bring-to-front">MOD</v-btn></v-list-item>
-        <v-list-item><v-btn @click="scrollTo('section2')" prepend-icon="mdi-store">OPTI</v-btn></v-list-item>
+        <v-list-item ><v-btn @click="scrollTo('section1')" prepend-icon="mdi-arrange-bring-to-front">Drawer</v-btn></v-list-item>
+        <v-list-item><v-btn @click="scrollTo('section2')" prepend-icon="mdi-store">Expanded & Hover</v-btn></v-list-item>
+        <v-list-item><v-btn @click="scrollTo('section3')" prepend-icon="mdi-store">Footer</v-btn></v-list-item>
         
       </v-list>
     </v-navigation-drawer>
     <!-- fin de navegador derecho -->
     <!-- ********************************** -->
-    <div id="section1" class="mt-12" style="height: 100vh;" >
+    <div id="section1" class="mt-12" >
       <v-row class="bg-red-lighten-4">
     <v-col cols="12" class="mt-6">
-      <h3 class="bg-blue pa-4  text-center">MODELOS DE PAGINAS COMPLETAS</h3>
+      <h3 class="bg-blue pa-4  text-center">NAVIGATION DRAWER & SCRIPT</h3>
     </v-col>
-        <!-- ****************************************************************************** -->
+        <!-- ************************************************************** -->
+      <v-col cols="12">
+        <v-card>
+    <v-layout>
+      <v-app-bar
+        color="primary"
+        prominent
+      >
+        <v-app-bar-nav-icon variant="text" @click.stop="drawers = !drawers"></v-app-bar-nav-icon>
+
+        <v-toolbar-title>ARCHIVOS</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <template v-if="$vuetify.display.mdAndUp">
+          <v-btn icon="mdi-magnify" variant="text"></v-btn>
+
+          <v-btn icon="mdi-filter" variant="text"></v-btn>
+        </template>
+
+        <v-btn icon="mdi-dots-vertical" variant="text"></v-btn>
+      </v-app-bar>
+
+      <v-navigation-drawer
+        v-model="drawers"
+        :location="$vuetify.display.mobile ? 'bottom' : undefined"
+        temporary
+      >
+        <v-list
+          :items="items"
+        ></v-list>
+      </v-navigation-drawer>
+
+      <v-main style="height: 500px;">
+        <v-card-text>
+          El cajón de navegación aparecerá desde la parte inferior en pantallas de menor tamaño. 
+        </v-card-text>
+      </v-main>
+    </v-layout>
+  </v-card>
+      </v-col>
+        <!-- ************************************************************** -->
         <v-col cols="3">
       <v-card
     class="mx-auto my-8"
     elevation="16"
     target="_blank"
-    href="http://127.0.0.1:5500/src/assets/vue2/ModelVue2.html"
+    href="http://127.0.0.1:5500/src/assets/vuetify/Drawer.html"
     max-width="344" >
     <v-card-item>
-      <v-card-title class="text-red"> CREACIÓN  </v-card-title>
-      <v-card-subtitle>Secuencia de Desarrollo</v-card-subtitle>
+      <v-card-title class="text-red"> DRAWER  </v-card-title>
+      <v-card-subtitle>VARIABLES EN sCRIPT</v-card-subtitle>
     </v-card-item>
     <v-card-text>
-   1. Model
-   2. Controller
-   3. Pages
-   4. Route
-   5. Component
-   6. Validaciones
+      Barra lateral que aparece al accionar botón
     </v-card-text>
   </v-card>
   </v-col>
       </v-row>
     </div>
-
       <!-- /////////////////////////////////////////////////////////////////////////////////////-->
       <div id="section2" style="height: 100vh;">
       <v-row class="my-12 text-center bg-teal-lighten-4">
   <v-col cols="12">
-    <h2 class="bg-pink-darken-4 pa-4">Vue2 OPTIONS </h2>
+    <h2 class="bg-pink-darken-4 pa-4">EXPANDED & HOVER </h2>
   </v-col>
+  <!-- ****************************************** -->
+   <v-col cols="12">
+    <v-card>
+    <v-layout>
+      <v-navigation-drawer
+        expand-on-hover
+        rail
+      >
+        <v-list>
+          <v-list-item
+            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+            subtitle="sandra_a88@gmailcom"
+            title="Sandra Adams"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
+          <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main style="height: 250px"></v-main>
+    </v-layout>
+  </v-card>
+   </v-col>
   <!-- ****************************************** -->
   <v-col cols="3">
     <v-card
     class="mx-auto my-8"
     elevation="16"
     target="_blank"
-    href="http://127.0.0.1:5500/src/assets/vue2/TemaMenu.html"
+    href="http://127.0.0.1:5500/src/assets/vuetify/Expanded.html"
     max-width="344" >
     <v-card-item>
-      <v-card-title> Color según Estado </v-card-title>
-      <v-card-subtitle>Color de acuerdo al cambio de estado</v-card-subtitle>
+      <v-card-title> Expanded & Hover </v-card-title>
+      <v-card-subtitle>Menu lateral </v-card-subtitle>
     </v-card-item>
-    <v-card-text>     Ver imagen
+    <v-card-text>
+      Aparece al poner el puntero sobre el menu     
     </v-card-text>
   </v-card>
 </v-col>
@@ -77,25 +143,48 @@
 </v-row>
 </div>
 <!-- ********************************************************************** -->
- <div id="section3" style="height: 100vh;">
+ <div id="section3" >
 <v-row class="bg-purple-lighten-4">
   <v-col cols="12">
-    <h2 class="bg-pink-darken-4 pa-4 text-center">FECHA</h2>
+    <h2 class="bg-pink-darken-4 pa-4 text-center">FOOTER</h2>
   </v-col>
+  <!-- ********************************************************************** -->
+   <v-col cols="12">
+    <v-footer class="d-flex flex-column">
+    <div class="bg-teal d-flex w-100 align-center px-4">
+      <strong>CONEXIÓN A REDES SOCIALES </strong>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        v-for="icon in icons"
+        :key="icon"
+        :icon="icon"
+        class="mx-4"
+        size="small"
+        variant="plain"
+      ></v-btn>
+    </div>
+
+    <div class="px-4 py-2 bg-black text-center w-100">
+      {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+    </div>
+  </v-footer>
+   </v-col>
   <!-- ********************************************************************** -->
   <v-col cols="3" >
     <v-card
     class="mx-auto my-8"
     elevation="16"
     target="_blank"
-    href="http://127.0.0.1:5500/src/assets/vue2/FechaSinhora.html"
+    href="http://127.0.0.1:5500/src/assets/vuetify/Footer.html"
     max-width="344" >
     <v-card-item>
-      <v-card-title> FECHA SIN HORA </v-card-title>
-      <v-card-subtitle> Eliminar la hora</v-card-subtitle>
+      <v-card-title> FOOTER</v-card-title>
+      <v-card-subtitle> Pie de página</v-card-subtitle>
     </v-card-item>
     <v-card-text>
-    fecha sin hora
+    Con iconos y enlaces a redes sociales
     </v-card-text>
   </v-card>
 </v-col>
@@ -106,7 +195,7 @@
 </template>
 <!-- ******************************* -->
 <script setup>
-  import { ref } from 'vue';
+  import { ref, watch } from 'vue';
 const drawer = ref(false);
 
 const toggleDrawer = () => {
@@ -120,8 +209,38 @@ const scrollTo = (sectionId) => {
     element.scrollIntoView({ behavior: 'smooth' });
   }
 };
+//nav drawer
+const drawers = ref(false);
+const group = ref(null);
+const items = [
+  {
+    title: 'Uno',
+    value: 'uno',
+  },
+  {
+    title: 'Dos',
+    value: 'dos',
+  },
+  {
+    title: 'Tres',
+    value: 'tres',
+  },
+  {
+    title: 'Cuatro',
+    value: 'cuatro',
+  },
+];
 
+watch(group, () => {
+  drawer.value = false;
+});
 
+const icons = ref([
+  'mdi-facebook',
+  'mdi-twitter',
+  'mdi-linkedin',
+  'mdi-instagram',
+])
 </script>
 <!-- ******************************* -->
 <style scoped>
