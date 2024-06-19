03adddb183300d5ee2e66dc1afd9375ae052d90f
nitram26 <carlosmartin1993@gmail.com>
2024-06-19 13:04:05 -0500

    cambios

--- src/views/Vue2View.vue
+++ src/views/Vue2View.vue
@@ -23,7 +23,7 @@
     </v-navigation-drawer>
     <!-- fin de navegador derecho -->
     <!-- ********************************** -->
-    <div id="section1" class="mt-12" style="height: 100vh;" >
+    <div id="section1" class="mt-12" >
       <v-row class="bg-red-lighten-4">
     <v-col cols="12" class="mt-6">
       <h3 class="bg-blue pa-4  text-center">MODELOS DE PAGINAS COMPLETAS</h3>
@@ -160,7 +160,7 @@
     </div>
 
       <!-- /////////////////////////////////////////////////////////////////////////////////////-->
-      <div id="section2" style="height: 100vh;">
+      <div id="section2">
       <v-row class="my-12 text-center bg-teal-lighten-4">
   <v-col cols="12">
     <h2 class="bg-pink-darken-4 pa-4">Vue2 OPTIONS </h2>
@@ -287,7 +287,7 @@
 </v-row>
 </div>
 <!-- ********************************************************************** -->
- <div id="section3" style="height: 100vh;">
+ <div id="section3">
 <v-row class="bg-purple-lighten-4">
   <v-col cols="12">
     <h2 class="bg-pink-darken-4 pa-4 text-center">FECHA</h2>
