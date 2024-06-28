0c2462c2b91ee4c05d79a9482c3aaa97d2840439
cmncp <carlos.cervera@istpmala.edu.pe>
2024-06-28 17:52:30 -0500

    reordenando images

--- src/views/WorkView.vue
+++ src/views/WorkView.vue
@@ -1,31 +1,13 @@
 <template>
   <div>
-   <h1 class="mb-12">VUE3</h1> 
-    <h2 :class="color">Este es una etiqueta que cambia de color </h2>
-
-    <v-btn @click="cambio">PULSAR</v-btn>
-   <br><br><br>
-  
+   <h1>VUE3</h1> 
   </div>
 </template>
 <!-- ******************************* --> 
 <script setup>
-import { ref } from 'vue';
-
-
-let color = ref('red')
-let cambio= ()=>{
-  color.value= 'danger' 
-}
 
 </script>
 <!-- ******************************* -->
 <style scoped>
-.danger{
-  color: red;
-}
-.red {
-  color: blue;
-}
-</style>
 
+</style>
--- src/views/front/Vue3View.vue
+++ src/views/front/Vue3View.vue
@@ -51,6 +51,23 @@ max-width="344" >
 class="mx-auto my-8"
 elevation="16"
 target="_blank"
+href="http://127.0.0.1:5501/src/assets/front/vue3/RelacionarComponentes.html"
+max-width="344" >
+<v-card-item>
+  <v-card-title> LLamar un Componente </v-card-title>
+  <v-card-subtitle>Relacionar 2 componentes</v-card-subtitle>
+</v-card-item>
+<v-card-text>
+Forma de unir y relacionar Componentes
+</v-card-text>
+</v-card>
+  </v-col>
+<!-- ********************************************************* -->
+  <v-col cols="3" class="bg-teal-lighten-5">
+    <v-card
+class="mx-auto my-8"
+elevation="16"
+target="_blank"
 href="http://127.0.0.1:5501/src/assets/studium/vue3/Snippet.html"
 max-width="344" >
 <v-card-item>
