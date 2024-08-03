8840577f626b74211a9612ac2e9bb0379e6c2855
cmncp <carlos.cervera@istpmala.edu.pe>
2024-08-03 17:59:44 -0500

    tipoDDato

--- src/views/front/JavascriptView.vue
+++ src/views/front/JavascriptView.vue
@@ -13,14 +13,77 @@
   <v-navigation-drawer v-model="drawer" class="br-teal-darken-4" theme="dark">
       <v-list>
         <v-list-item link to="/" title="HOME" prepend-icon="mdi-grid"></v-list-item>
-        <v-list-item ><v-btn @click="scrollTo('section1')" prepend-icon="mdi-arrange-bring-to-front">FECHA Y HORA</v-btn></v-list-item>
+        <!-- secciones++++++++++++++++++++++++++++++++++++++++ -->
+        <v-list-item ><v-btn @click="scrollTo('tipoDatos')" prepend-icon="mdi-arrange-bring-to-front">TIPO DE DATOS</v-btn></v-list-item>
+        <v-list-item ><v-btn @click="scrollTo('fecha')" prepend-icon="mdi-arrange-bring-to-front">FECHA Y HORA</v-btn></v-list-item>
         <v-list-item><v-btn @click="scrollTo('section2')" prepend-icon="mdi-store">OPTIONS</v-btn></v-list-item>
         
       </v-list>
     </v-navigation-drawer>
     <!-- fin de navegador derecho -->
-    <!-- ********************************** -->
-    <div id="section1" class="mt-12" >
+    <!--///////////////////////////////////////////////////////////////////////// -->
+    <div id="tipoDatos" class="mt-12" >
+      <v-row class="bg-red-lighten-4">
+    <v-col cols="12" class="mt-6">
+      <h3 class="bg-blue pa-4  text-center">TIPO DE DATOS</h3>
+    </v-col>
+        <!-- ****************************************************************************** -->
+        <v-col cols="3">
+      <v-card
+    class="mx-auto my-8"
+    elevation="16"
+    target="_blank"
+    href="http://127.0.0.1:5501/src/assets/front/javascript/datoString.html"
+    max-width="344" >
+    <v-card-item>
+      <v-card-title class="text-red"> INTRODUCCIÓN  </v-card-title>
+      <v-card-subtitle>Escritura y Ordenamiento del Código</v-card-subtitle>
+    </v-card-item>
+    <v-card-text>
+     Extensiones Recomendadas
+    </v-card-text>
+  </v-card>
+  </v-col>
+<!-- ************************************************************** -->
+        <v-col cols="3">
+      <v-card
+    class="mx-auto my-8"
+    elevation="16"
+    target="_blank"
+    href="http://127.0.0.1:5501/src/assets/front/javascript/tipoString.html"
+    max-width="344" >
+    <v-card-item>
+      <v-card-title class="text-red"> STRING  </v-card-title>
+      <v-card-subtitle>Cadenas de Texto</v-card-subtitle>
+    </v-card-item>
+    <v-card-text>
+    Propiedades y Métodos  
+    </v-card-text>
+  </v-card>
+  </v-col>
+  <!-- ************************************************************** -->
+
+        <v-col cols="3">
+      <v-card
+    class="mx-auto my-8"
+    elevation="16"
+    target="_blank"
+    href="http://127.0.0.1:5501/src/assets/front/javascript/datoNumber.html"
+    max-width="344" >
+    <v-card-item>
+      <v-card-title class="text-red"> NUMBER  </v-card-title>
+      <v-card-subtitle>Cadenas de Texto</v-card-subtitle>
+    </v-card-item>
+    <v-card-text>
+    Propiedades y Métodos  
+    </v-card-text>
+  </v-card>
+  </v-col>
+  <!-- ************************************************************** -->
+      </v-row>
+    </div>
+      <!-- ////////////////////////////////////////////////////////-->
+    <div id="fecha" class="mt-12" >
       <v-row class="bg-red-lighten-4">
     <v-col cols="12" class="mt-6">
       <h3 class="bg-blue pa-4  text-center">FECHA Y HORA</h3>
@@ -34,22 +97,22 @@
     href="http://127.0.0.1:5501/src/assets/front/javascript/FechaActual.html"
     max-width="344" >
     <v-card-item>
-      <v-card-title class="text-red"> FECHA ACTUAL  </v-card-title>
+      <v-card-title class="text-purple"> FECHA ACTUAL  </v-card-title>
       <v-card-subtitle>traer la fecha y hora actual</v-card-subtitle>
     </v-card-item>
     <v-card-text>
-     Formater para quedarse con la fecha DD/MM/YYYY
+     Formatear para quedarse con la fecha DD/MM/YYYY
     </v-card-text>
   </v-card>
   </v-col>
       </v-row>
     </div>
 
-      <!-- /////////////////////////////////////////////////////////////////////////////////////-->
-      <div id="section2">
+      <!-- ////////////////////////////////////////////////////////-->
+      <div id="section">
       <v-row class="my-12 text-center bg-teal-lighten-4">
   <v-col cols="12">
-    <h2 class="bg-pink-darken-4 pa-4">Vue2 OPTIONS </h2>
+    <h2 class="bg-pink-darken-4 pa-4">OPTIONS </h2>
   </v-col>
   <!-- ****************************************** -->
   <v-col cols="3">
@@ -71,8 +134,8 @@
 <!-- ********************************************************************** -->
 </v-row>
 </div>
-<!-- ********************************************************************** -->
- <div id="section3">
+<!-- ////////////////////////////////////////////////////////////////////-->
+ <div id="section">
 <v-row class="bg-purple-lighten-4">
   <v-col cols="12">
     <h2 class="bg-pink-darken-4 pa-4 text-center">otro</h2>
