7eef6ef638aca4b046da11e4adc2ac52c4099a70
cmncp <carlos.cervera@istpmala.edu.pe>
2024-09-20 16:24:25 -0500

    project

--- src/views/proyectos/ProjectView.vue
+++ src/views/proyectos/ProjectView.vue
@@ -1,14 +1,100 @@
+
+
 <template>
+  <v-row class="bg-teal-darken-1 pa-2 mt-12 fixed-top" >
+      <v-col cols="11" class="text-center">
+          <h2 >EDUCARE </h2>
+    </v-col>
+    <v-col cols="1" >
+<v-btn icon @click="toggleDrawer" >
+    <v-img src="@/assets/image/CERVERA.png" alt="My Image2" style="width: 39px; border-radius: 1em;"/></v-btn>
+    </v-col>
+  </v-row>
+  <!-- ********************************************* -->
+  <v-navigation-drawer v-model="drawer" class="br-teal-darken-4" theme="dark">
+      <v-list>
+        <v-list-item link to="/" title="HOME" prepend-icon="mdi-grid"></v-list-item>
+        <v-list-item link to="/front" title="Containment" prepend-icon="mdi-arrange-bring-to-front"></v-list-item>
+        <v-list-item link to="/vue2" title="Navigation" prepend-icon="mdi-arrange-send-to-back"></v-list-item>
+        <v-list-item link to="/store" title="Form, inputs & controls" prepend-icon="mdi-database-cog-outline"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <!-- ************************************************************ -->
+  <v-row class="mt-12 ">
+    <v-col class="mt-4">
+      <h3 class="bg-purple pa-4  text-center">VISIÓN </h3>
+    </v-col>
+      </v-row>
+      <!-- fin de navegador derecho -->
+      <v-row class="bg-purple-lighten-4">
+        <!-- ****************************************************** -->
+        <v-col cols="3">
+      <v-card
+    class="mx-auto my-8"
+    elevation="16"
+    target="_blank"
+    href="http://127.0.0.1:5501/src/assets/proyectos/vision.htm"
+    max-width="344" >
+    <v-card-item>
+      <v-card-title class="text-indigo"> VISION </v-card-title>
+      <v-card-subtitle>Secuencia de Desarrollo</v-card-subtitle>
+    </v-card-item>
+    <v-card-text>
+      Model
+    </v-card-text>
+  </v-card>
+  </v-col>
+       <!-- ****************************************************** -->
+  </v-row>
+  <v-row class="mt-12 ">
+    <v-col class="mt-4">
+      <h3 class="bg-indigo pa-4  text-center">MISIÓN </h3>
+    </v-col>
+      </v-row>
+      <!-- fin de navegador derecho -->
+      <v-row class="bg-indigo-lighten-4">
+        <!-- ****************************************************** -->
+        <v-col cols="3">
+      <v-card
+    class="mx-auto my-8"
+    elevation="16"
+    target="_blank"
+    href="http://127.0.0.1:5501/src/assets/proyectos/mision.html"
+    max-width="344" >
+    <v-card-item>
+      <v-card-title class="text-indigo"> CREACIÓN  </v-card-title>
+      <v-card-subtitle>Secuencia de Desarrollo</v-card-subtitle>
+    </v-card-item>
+    <v-card-text>
+      Model
+    </v-card-text>
+  </v-card>
+  </v-col>
+       <!-- ****************************************************** -->
+  </v-row>
   
-  <v-app>
-    <v-main>
-      <DataTable />
-    </v-main>
-  </v-app>
 </template>
+<!-- //////////////////////////////////////////////////////////////////-->
 
 <script setup>
-import DataTable from '@/components/DataTable.vue';
-</script>
+  import { ref } from 'vue';
+const drawer = ref(false);
 
+const toggleDrawer = () => {
+  drawer.value = !drawer.value;
+}
+</script>
+<!-- ******************************* -->
+<style scoped>
+.fixed-top {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 101%;
+  z-index: 1000
+}
+
+</style>
   
+
+
