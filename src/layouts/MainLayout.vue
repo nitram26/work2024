56d5f3e12a113a0a43b9dfc04767bbd4f8d34550
nitram26 <carlosmartin1993@gmail.com>
2024-09-26 12:25:36 -0500

    2409261225

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -6,15 +6,24 @@
     <v-col cols="1" >
 
 <v-btn icon @click="toggleDrawer" >
-    <v-img src="@/assets/image/CERVERA.png" alt="My Image2" style="width: 39px; border-radius: 1em;"/></v-btn>
+    <v-img src="@/assets/image/CERVERA.png" alt="My Image2" class="img"/></v-btn>
     </v-col>
   </v-row>
+  
   <v-navigation-drawer v-model="drawer" class="br-teal-darken-4" theme="dark">
+    <div class="uno">
+      <v-img
+        src="@/assets/image/CERVERA.png"
+        max-width="40"
+        class="img"
+      > <a href="/graph" class=""></a></v-img>
+      <slot name="image"></slot>
+    </div>
       <v-list>
-        <v-list-item link to="/" title="TEMAS" prepend-icon="mdi-grid"></v-list-item>
         <v-list-item link to="/graph" title="GraphQL" prepend-icon="mdi-arrange-bring-to-front"></v-list-item>
-        </v-list>
+      </v-list>
     </v-navigation-drawer>
+  
 </template>
 <!-- ************************************* -->
 <script setup>
@@ -27,5 +36,11 @@ const toggleDrawer = () => {
 </script>
 <!-- ************************************* -->
 <style scoped>
-
+.img{
+  width: 40px;
+  border-radius: 10px;
+}
+.uno{
+  margin: 1em;
+}
 </style>
