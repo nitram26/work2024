1e040dd7d7ad7b4d49c0716edf426fc6ed57b091
cmncp <carlosmcervera1993@gmail.com>
2024-09-26 17:23:22 -0500

    2409261723

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -9,11 +9,35 @@
     <v-img src="@/assets/image/CERVERA.png" alt="My Image2" style="width: 39px; border-radius: 1em;"/></v-btn>
     </v-col>
   </v-row>
+
   <v-navigation-drawer v-model="drawer" class="br-teal-darken-4" theme="dark">
-      <v-list>
-        <v-list-item link to="/" title="TEMAS" prepend-icon="mdi-grid"></v-list-item>
-        <v-list-item link to="/graph" title="GraphQL" prepend-icon="mdi-arrange-bring-to-front"></v-list-item>
-        </v-list>
+    <div class="container">
+      <div class="dos">
+      <v-img src="@/assets/image/CERVERA.png" class="img"></v-img>
+      <slot name="image1"></slot>
+    </div>
+      <div class="dos">
+      <v-img src="@/assets/image/CERVERA.png" class="img"></v-img>
+      <slot name="image2"></slot>
+    </div>
+      <div class="dos">
+      <v-img src="@/assets/image/CERVERA.png" class="img"></v-img>
+      <slot name="image3"></slot>
+    </div>
+      <div class="dos">
+      <v-img src="@/assets/image/CERVERA.png" class="img"></v-img>
+      <slot name="image4"></slot>
+    </div>
+      <div class="dos">
+      <v-img src="@/assets/image/CERVERA.png" class="img"></v-img>
+      <slot name="image5"></slot>
+    </div>
+      <div class="dos">
+      <v-img src="@/assets/image/CERVERA.png" class="img"></v-img>
+      <slot name="image6"></slot>
+    </div>
+    </div>
+
     </v-navigation-drawer>
 </template>
 <!-- ************************************* -->
@@ -24,8 +48,24 @@ const drawer = ref(false);
 const toggleDrawer = () => {
   drawer.value = !drawer.value;
 }
+
 </script>
 <!-- ************************************* -->
 <style scoped>
+.img{
+  max-width: 40px;
+  border-radius: 10px;
+  margin-left: 1em;
+  
+}
+.container{
+display: block;
+
+}
+.dos{
+  display: flex;
+  padding: 1em;
+}
+
 
 </style>
--- src/views/back/backendView.vue
+++ src/views/back/backendView.vue
@@ -1,19 +1,17 @@
 <template>
  <MainLayout>
     <template #main> <h1>BACK</h1> </template>
+    <template #image1>  <a href="/graph" class="lat">GRAPH</a> </template>
+    <template #image2>  <a href="/graph" class="lat">SQL Server</a> </template>
+    <template #image3>  <a href="/graph" class="lat">MongoDB</a> </template>
+    <template #image4>  <a href="/graph" class="lat">Sequlize</a> </template>
+    <template #image5>  <a href="/graph" class="lat">PhpMyAdmin</a> </template>
+    <template #image6>  <a href="/graph" class="lat">SQL</a> </template>
+   
+     
  </MainLayout>
   <!-- ********************************************* -->
-  <v-navigation-drawer v-model="drawer" class="br-teal-darken-4" theme="dark">
-      <v-list>
-        <v-list-item link to="/" title="TEMAS" prepend-icon="mdi-grid"></v-list-item>
-        <v-list-item link to="/front" title="Vue2" prepend-icon="mdi-arrange-bring-to-front"></v-list-item>
-        <v-list-item link to="/vue2" title="dos" prepend-icon="mdi-arrange-send-to-back"></v-list-item>
-        <v-list-item link to="/store" title="tres" prepend-icon="mdi-database-cog-outline"></v-list-item>
-        <v-list-item link to="/backend" title="cuatro" prepend-icon="mdi-store"></v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <!-- ********************************** -->
-  <v-row class="mt-12">
+   <v-row class="mt-12">
     <v-col class="mt-4">
       <h3 class="bg-blue pa-4  text-center">MODELOS </h3>
     </v-col>
@@ -44,22 +42,19 @@
 
 <script setup>
   import MainLayout from '@/layouts/MainLayout.vue';
-// import { ref } from 'vue';
-// const drawer = ref(false);
 
-// const toggleDrawer = () => {
-//   drawer.value = !drawer.value;
-// }
 </script>
 <!-- ******************************* -->
 <style scoped>
-.fixed-top {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 101%;
-  z-index: 1000
+h6{
+  margin: 1em;
 }
 
+.lat{
+  margin-left: 3em;
+  margin-top: 1em;
+  color: white;
+
+}
 </style>
   
