a81043f617e3c63260ff62ea5391187efb343ee0
nitram26 <carlosmartin1993@gmail.com>
2024-09-27 09:14:27 -0500

    2409270914

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,61 +1,44 @@
 <template>
-     <v-row class="bg-teal-darken-2 pa-2" >
-      <v-col cols="11" class="text-center">
-          <slot name="main"></slot>
-    </v-col>
-    <v-col cols="1" >
+  <v-row class="bg-teal-darken-2 pa-2" >
+   <v-col cols="11" class="text-center">
+       <slot name="main"></slot>
+ </v-col>
+ <v-col cols="1" >
 
 <v-btn icon @click="toggleDrawer" >
-    <v-img src="@/assets/image/CERVERA.png" alt="My Image2" class="img"/></v-btn>
-    </v-col>
-  </v-row>
-<<<<<<< HEAD
-  
-  <v-navigation-drawer v-model="drawer" class="br-teal-darken-4" theme="dark">
-    <div class="uno">
-      <v-img
-        src="@/assets/image/CERVERA.png"
-        max-width="40"
-        class="img"
-      > <a href="/graph" class=""></a></v-img>
-      <slot name="image"></slot>
-    </div>
-      <v-list>
-        <v-list-item link to="/graph" title="GraphQL" prepend-icon="mdi-arrange-bring-to-front"></v-list-item>
-      </v-list>
-=======
+ <v-img src="@/assets/image/CERVERA.png" alt="My Image2" style="width: 39px; border-radius: 1em;"/></v-btn>
+ </v-col>
+</v-row>
 
-  <v-navigation-drawer v-model="drawer" class="br-teal-darken-4" theme="dark">
-    <div class="container">
-      <div class="dos">
-      <v-img src="@/assets/image/CERVERA.png" class="img"></v-img>
-      <slot name="image1"></slot>
-    </div>
-      <div class="dos">
-      <v-img src="@/assets/image/CERVERA.png" class="img"></v-img>
-      <slot name="image2"></slot>
-    </div>
-      <div class="dos">
-      <v-img src="@/assets/image/CERVERA.png" class="img"></v-img>
-      <slot name="image3"></slot>
-    </div>
-      <div class="dos">
-      <v-img src="@/assets/image/CERVERA.png" class="img"></v-img>
-      <slot name="image4"></slot>
-    </div>
-      <div class="dos">
-      <v-img src="@/assets/image/CERVERA.png" class="img"></v-img>
-      <slot name="image5"></slot>
-    </div>
-      <div class="dos">
-      <v-img src="@/assets/image/CERVERA.png" class="img"></v-img>
-      <slot name="image6"></slot>
-    </div>
-    </div>
+<v-navigation-drawer v-model="drawer" class="br-teal-darken-4" theme="dark">
+ <div class="container">
+   <div class="dos">
+   <v-img src="@/assets/image/CERVERA.png" class="img"></v-img>
+   <slot name="image1"></slot>
+ </div>
+   <div class="dos">
+   <v-img src="@/assets/image/CERVERA.png" class="img"></v-img>
+   <slot name="image2"></slot>
+ </div>
+   <div class="dos">
+   <v-img src="@/assets/image/CERVERA.png" class="img"></v-img>
+   <slot name="image3"></slot>
+ </div>
+   <div class="dos">
+   <v-img src="@/assets/image/CERVERA.png" class="img"></v-img>
+   <slot name="image4"></slot>
+ </div>
+   <div class="dos">
+   <v-img src="@/assets/image/CERVERA.png" class="img"></v-img>
+   <slot name="image5"></slot>
+ </div>
+   <div class="dos">
+   <v-img src="@/assets/image/CERVERA.png" class="img"></v-img>
+   <slot name="image6"></slot>
+ </div>
+ </div>
 
->>>>>>> 1e040dd7d7ad7b4d49c0716edf426fc6ed57b091
-    </v-navigation-drawer>
-  
+ </v-navigation-drawer>
 </template>
 <!-- ************************************* -->
 <script setup>
@@ -63,35 +46,26 @@ import { ref } from 'vue';
 const drawer = ref(false);
 
 const toggleDrawer = () => {
-  drawer.value = !drawer.value;
+drawer.value = !drawer.value;
 }
 
 </script>
 <!-- ************************************* -->
 <style scoped>
 .img{
-<<<<<<< HEAD
-  width: 40px;
-  border-radius: 10px;
-}
-.uno{
-  margin: 1em;
-}
-=======
-  max-width: 40px;
-  border-radius: 10px;
-  margin-left: 1em;
-  
+max-width: 40px;
+border-radius: 10px;
+margin-left: 1em;
+
 }
 .container{
 display: block;
 
 }
 .dos{
-  display: flex;
-  padding: 1em;
+display: flex;
+padding: 1em;
 }
 
 
->>>>>>> 1e040dd7d7ad7b4d49c0716edf426fc6ed57b091
 </style>
--- src/views/back/backendView.vue
+++ src/views/back/backendView.vue
@@ -1,60 +1,60 @@
 <template>
- <MainLayout>
-    <template #main> <h1>BACK</h1> </template>
- </MainLayout>
-  <!-- ********************************************* -->
-  <v-navigation-drawer v-model="drawer" class="br-teal-darken-4" theme="dark">
-      <v-list>
-        <v-list-item link to="/" title="TEMAS" prepend-icon="mdi-grid"></v-list-item>
-        <v-list-item link to="/front" title="Vue2" prepend-icon="mdi-arrange-bring-to-front"></v-list-item>
-        <v-list-item link to="/vue2" title="dos" prepend-icon="mdi-arrange-send-to-back"></v-list-item>
-        <v-list-item link to="/store" title="tres" prepend-icon="mdi-database-cog-outline"></v-list-item>
-        <v-list-item link to="/backend" title="cuatro" prepend-icon="mdi-store"></v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <!-- ********************************** -->
-  <v-row class="mt-12">
-    <v-col class="mt-4">
-      <h3 class="bg-blue pa-4  text-center">MODELOS </h3>
-    </v-col>
-     
-      </v-row>
-      <!-- fin de navegador derecho -->
-      <v-row class="bg-red-lighten-4">
-        <!-- ****************************************************************************** -->
-        <v-col cols="3">
-      <v-card
-    class="mx-auto my-8"
-    elevation="16"
-    target="_blank"
-    href="http://127.0.0.1:5501/src/assets/vue2/ModelVue2.html"
-    max-width="344" >
-    <v-card-item>
-      <v-card-title class="text-red"> CREACIÓN  </v-card-title>
-      <v-card-subtitle>Secuencia de Desarrollo</v-card-subtitle>
-    </v-card-item>
-    <v-card-text>
-    Model
-    </v-card-text>
-  </v-card>
-  </v-col>
-  </v-row>
-</template>
-<!-- /////////////////////////////////////////////////////////////////////////////////////-->
-
-<script setup>
-  import MainLayout from '@/layouts/MainLayout.vue';
-
-</script>
-<!-- ******************************* -->
-<style scoped>
-.fixed-top {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 101%;
-  z-index: 1000
-}
-
-</style>
-  
+  <MainLayout>
+     <template #main> <h1>BACK</h1> </template>
+     <template #image1>  <a href="/graph" class="lat">GRAPH</a> </template>
+     <template #image2>  <a href="/graph" class="lat">SQL Server</a> </template>
+     <template #image3>  <a href="/graph" class="lat">MongoDB</a> </template>
+     <template #image4>  <a href="/graph" class="lat">Sequlize</a> </template>
+     <template #image5>  <a href="/graph" class="lat">PhpMyAdmin</a> </template>
+     <template #image6>  <a href="/graph" class="lat">SQL</a> </template>
+    
+      
+  </MainLayout>
+   <!-- ********************************************* -->
+    <v-row class="mt-12">
+     <v-col class="mt-4">
+       <h3 class="bg-blue pa-4  text-center">MODELOS </h3>
+     </v-col>
+      
+       </v-row>
+       <!-- fin de navegador derecho -->
+       <v-row class="bg-red-lighten-4">
+         <!-- ****************************************************************************** -->
+         <v-col cols="3">
+       <v-card
+     class="mx-auto my-8"
+     elevation="16"
+     target="_blank"
+     href="http://127.0.0.1:5501/src/assets/vue2/ModelVue2.html"
+     max-width="344" >
+     <v-card-item>
+       <v-card-title class="text-red"> CREACIÓN  </v-card-title>
+       <v-card-subtitle>Secuencia de Desarrollo</v-card-subtitle>
+     </v-card-item>
+     <v-card-text>
+     Model
+     </v-card-text>
+   </v-card>
+   </v-col>
+   </v-row>
+ </template>
+ <!-- /////////////////////////////////////////////////////////////////////////////////////-->
+ 
+ <script setup>
+   import MainLayout from '@/layouts/MainLayout.vue';
+ 
+ </script>
+ <!-- ******************************* -->
+ <style scoped>
+ h6{
+   margin: 1em;
+ }
+ 
+ .lat{
+   margin-left: 3em;
+   margin-top: 1em;
+   color: white;
+ 
+ }
+ </style>
+   
